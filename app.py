479c30f137af4db16244280fc1d785c7cb89cacd
NikolaiHoj <Nikolaih1603@gmail.com>
2021-08-24 18:43:43 +0200

    canvas server og client + fjernede node_modules

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        data = request.json
+        print("data is " + format(data))
+        return jsonify({"predicted": "1"})
+    
+    return "Invalid request type"
 
 @app.route('/favicon.ico')
 def favicon():
--- static/js/index.js
+++ static/js/index.js
@@ -1,46 +1,68 @@
-var canvas = document.getElementById('drawing');
-var ctx = canvas.getContext('2d');
+let canvas = document.getElementById("drawing");
+let ctx = canvas.getContext("2d");
 
 // last known position
 let pos = { x: 0, y: 0 };
 
 // new position from mouse event
 function setPosition(e) {
-  const rect = canvas.getBoundingClientRect();
-  pos.x = (e.clientX - rect.left);
-  pos.y = (e.clientY - rect.top);
-  
-  // console.log(`x: ${pos.x}, y: ${pos.y}`);
+	const rect = canvas.getBoundingClientRect();
+	pos.x = (e.clientX - rect.left) / ((rect.bottom - rect.top) / canvas.height);
+	pos.y = (e.clientY - rect.top) / ((rect.bottom - rect.top) / canvas.height);
+
+	// console.log(`x: ${pos.x}, y: ${pos.y}`);
 }
 
 function draw(e) {
-  if (e.buttons !== 1) return;
-  
-  ctx.beginPath(); // begin
-  
-  ctx.lineWidth = 5;
-  ctx.lineCap = 'square';
-  ctx.lineJoin = 'square';
-  ctx.strokeStyle = 'black';
-  
-  ctx.moveTo(pos.x, pos.y); // draw from this position (last position)
-  setPosition(e); // update position
-  ctx.lineTo(pos.x, pos.y); // draw to this position (current position)
-  
-  ctx.stroke();
+	if (e.buttons !== 1) return;
+
+	ctx.beginPath(); // begin
+
+	ctx.lineWidth = 1;
+	ctx.lineCap = "square";
+	ctx.lineJoin = "square";
+	ctx.strokeStyle = "black";
+
+	ctx.moveTo(pos.x, pos.y); // draw from this position (last position)
+	setPosition(e); // update position
+	ctx.lineTo(pos.x, pos.y); // draw to this position (current position)
+
+	ctx.stroke();
 }
 
 function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-document.addEventListener('mousemove', draw);
-document.addEventListener('mousedown', setPosition);
-document.addEventListener('mouseenter', setPosition);
-// document.addEventListener('clear', clearCanvas);
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mouseenter", setPosition);
 
-// function clearCanvas(whenPressKey) {
-//   if (whenPressKey.keyCode == key.C) {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   }
-// }
+const submitButton = document.getElementById("submit");
+
+submitButton.onclick = (e) => {
+	e.preventDefault();
+	const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+	const pixels = imgData.data;
+	console.log("pixels", pixels);
+	let grayscale = [];
+
+	// get lightness of each pixel
+	for (let i = 0; i < pixels.length; i += 4) {
+		// const lightness = parseInt((pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3);
+		grayscale.push(pixels[3] / 255);
+	}
+
+	console.log("grayscale", grayscale);
+
+	fetch("/upload", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(grayscale),
+	}).then((res) => {
+		console.log("Request complete! response:", res);
+	});
+};
